1d6b3c6f3f2cc1b37010f983e6db4454ca7800f9
Ramesesiv <x.j.sutherland@gmail.com>
2022-02-17 04:58:25 -0600

    Update to Delete button, Not sure how to do HTTP request, CRUD IS CRUD LOL

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const url='http://localhost:3000/notes/notes'
+const url= 'http://localhost:3000/notes'
 const addBtn = document.getElementById("add");
-const notes = document.getElementById('notes');
+const notes = document.getElementById("notes")
 
 if (notes) {
     notes.forEach((note) => {
@@ -8,22 +8,130 @@ if (notes) {
     });
 }
 
-addBtn.addEventListener("click", () => {
+
+
+function myNotes(){
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+    for (let noteObj of data) {
+        renderNoteItem(noteObj)
+    }
+    })
+
+}
+function updateNote(element) {  
+    const noteTitle = document.querySelector('NoteTitle').value 
+    const noteBody = document.querySelector('textarea').value
+    fetch(`http://localhost:3000/notes/${noteId}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            title: noteTitle,
+            body: noteBody,
+            
+            // updated_at: moment().format(),
+        }),
+    })
+    .then(function (res) {
+        return res.json()
+    })
+    .then(function (data) {
+        console.log(data)
+        renderNoteText(element.parentElement, data)
+    })
+
+}
+function createNote(noteBody){
+    fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          item: todoText,
+          created_at: moment().format(),
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          renderTodoItem(data)
+        })
+      clearInputs()
+
+}
+addBtn.addEventListener("click", () => 
+{
+
     addNewNote();
 });
 
+
+
+const main = note.querySelector(".main");
+const Notetitle= note.querySelector("Notetitle")
+const textArea = note.querySelector("textarea");
+
+textArea.value = text;
+main.innerHTML = marked(text);
+
+
+
+
+function deleteNote(element){
+    const todoId = element.parentElement.id
+    fetch(`http://localhost:3000/todos/${todoId}`, {
+      method: 'DELETE',
+    }).then(function () {
+        deleteBtn.addEventListener("click", () => {
+            const deleteBtn = note.querySelector(".delete");
+        
+            note.remove();
+        
+            updateLS();
+        });
+      
+    })
+
+  
+}
+function updateTodo(element){
+
+
+}
+function updateNotes(element){
+    const editBtn = note.querySelector(".edit");
+    const NotesId=element.parentElement.id
+
+//fetch('')
+
+
+/*editBtn.addEventListener("click", () => {
+  
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+});
+*/
+
+}
+
+textArea.addEventListener("input", (e) => {
+    
+    const { value } = e.target;
+
+    main.innerHTML = marked(value);
+
+    updateLS();
+});
+
+
 function addNewNote(text = "") {
     const note = document.createElement("div");
-    note.classList.add("note");  
-    /*let now = new Date();
-    const DateTime = note.querySelector(".dateTime")
-    let dateTime = `${now.getDate()}-${now.getMonth()+1}-${now.getFullYear()} | ${now.getHours()}:${now.getMinutes()}`;
-    */
+    note.classList.add("note");
+
     note.innerHTML = `
         <div class="notes">
             <div class="tools">
-                <div id="dateTime">
-                </div>
+            <div id="Notetitle"></div>
+            <div id="dateTime"></div>
                 <button class="edit"><i class="fas fa-edit"></i></button>
                 <button class="delete"><i class="fas fa-trash-alt"></i></button>
             </div>
@@ -32,41 +140,7 @@ function addNewNote(text = "") {
         </div>
     `;
 
-    const editBtn = note.querySelector(".edit");
-    const deleteBtn = note.querySelector(".delete");
-
-    const main = note.querySelector(".main");
-    const textArea = note.querySelector("textarea");
-
-   
   
-
-    textArea.value = text;
-    main.innerHTML = marked(text);
-
-
-
-
-    editBtn.addEventListener("click", () => {
-        main.classList.toggle("hidden");
-        textArea.classList.toggle("hidden");
-    });
-
-    deleteBtn.addEventListener("click", () => {
-        note.remove();
-
-        updateLS();
-    });
-
-   
-
-    textArea.addEventListener("input", (e) => {
-        const { value } = e.target;
-
-        main.innerHTML = marked(value);
-
-        updateLS();
-    });
 
     document.body.appendChild(note);
 }
